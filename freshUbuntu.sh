2dfd26a27627926808825c7dedf8d7987aaeffb0
Slimane <selmo47@gmail.com>
2017-02-07 15:16:09 +0100

    First update and  setup script
    	that includes:
    	system update & upgrade
    	and the following program installation
    		Multimedia: vlc
    		network & Inetrnet tools: skype, youtube-dl
    		Developpement tools: git, default-jdk
    		Virtualbox
    		Usefull system tools: gparted, htop
    		Windows emulator & font: wine msttcorefonts
    	Updating README file by adding the above description
    	Adding .commit_message file
    	Adding .gitignore file that contains (.commit_message)

---
+++ freshUbuntu.sh
@@ -0,0 +1,35 @@
+## I. Updating lists:
+apt update
+apt upgrade -y
+apt dist-upgrade -y
+
+##Media codecs
+apt install -y ubuntu-restricted-extras
+
+#Multimedia
+sudo apt install -y vlc
+#network & Inetrnet tools
+apt install -y skype
+apt install -y youtube-dl
+
+## Developpement tools
+
+#git: Most famous versionnoing Control System (VCS)
+apt install -y git
+
+# open-jdk
+apt install -y default-jdk
+
+#Virtualbox
+apt-get install -y virtualbox virtualbox-qt virtualbox-dkms
+
+## Usefull system tools
+apt install -y gparted htop
+
+## Ms Windows tools
+# Emulators
+#wine suite
+apt install -y wine winetricks
+
+# Windows fonts
+apt install msttcorefonts -y
