c63b10cae5522c8982a477e078de202a0626ca44
hd2fik <toufik.hadjsaid@etu.usthb.dz>
2017-10-31 22:21:11 +0100

    add node js
    add node js

--- freshUbuntu.sh
+++ freshUbuntu.sh
@@ -33,3 +33,7 @@ apt install -y wine winetricks
 
 # Windows fonts
 apt install msttcorefonts -y
+
+# Install node js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
